fc1a2282e677315a175cfcc25e367f12128f8ed7
Student <student@LAB308-27.local>
2017-08-23 15:44:50 +0700

    sent homework

---
+++ lab02_homework/Lab02/app/src/main/java/com/example/student/lab02/Lab02_HW.java
@@ -0,0 +1,13 @@
+package com.example.student.lab02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab02_HW extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab02_hw2);
+    }
+}
