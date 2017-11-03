2f17a0769e3a94bfd913e77e96aae80126f14338
Student <student@LAB308-27.it.kmitl.ac.th>
2017-11-03 12:05:34 +0700

    add Lab09

---
+++ Lab09/app/src/main/java/com/example/student/lab09/MessageDB.java
@@ -0,0 +1,13 @@
+package com.example.student.lab09;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+/**
+ * Created by student on 11/3/2017 AD.
+ */
+@Database(entities = {MessageInfo.class}, version = 1)
+
+public abstract class MessageDB extends RoomDatabase{
+    public abstract MessageInfoDAO getMessageInfoDAO();
+}
