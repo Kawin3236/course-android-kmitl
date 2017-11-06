441e547cacbcf5ecc464d1b491566e51b7079bd3
kawin3236 <58070006@kmitl.ac.th>
2017-11-06 20:26:45 +0700

    edit code

--- HoYuNi/HorYuNi2/app/src/main/java/kmitl/kawin58070006/horyuni/ImageUpload.java
+++ HoYuNi/HorYuNi2/app/src/main/java/kmitl/kawin58070006/horyuni/ImageUpload.java
@@ -8,6 +8,14 @@ public class ImageUpload {
 
     public String name;
     public String url;
+    public String url2;
+    public String url3;
+    public String url4;
+    public String url5;
+    public String url6;
+    public String url7;
+    public String url8;
+
 
     public String getName() {
         return name;
@@ -17,9 +25,74 @@ public class ImageUpload {
         return url;
     }
 
-    public ImageUpload(String name, String url) {
+    public ImageUpload(String name, String url, String url2) {
         this.name = name;
         this.url = url;
+        this.url2 = url2;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl2() {
+        return url2;
+    }
+
+    public void setUrl2(String url2) {
+        this.url2 = url2;
+    }
+
+    public String getUrl3() {
+        return url3;
+    }
+
+    public void setUrl3(String url3) {
+        this.url3 = url3;
+    }
+
+    public String getUrl4() {
+        return url4;
+    }
+
+    public void setUrl4(String url4) {
+        this.url4 = url4;
+    }
+
+    public String getUrl5() {
+        return url5;
+    }
+
+    public void setUrl5(String url5) {
+        this.url5 = url5;
+    }
+
+    public String getUrl6() {
+        return url6;
+    }
+
+    public void setUrl6(String url6) {
+        this.url6 = url6;
+    }
+
+    public String getUrl7() {
+        return url7;
+    }
+
+    public void setUrl7(String url7) {
+        this.url7 = url7;
+    }
+
+    public String getUrl8() {
+        return url8;
+    }
+
+    public void setUrl8(String url8) {
+        this.url8 = url8;
     }
 
     public ImageUpload(){}
--- HoYuNi/HorYuNi2/app/src/main/java/kmitl/kawin58070006/horyuni/MainFragment.java
+++ HoYuNi/HorYuNi2/app/src/main/java/kmitl/kawin58070006/horyuni/MainFragment.java
@@ -30,6 +30,8 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -41,8 +43,10 @@ public class MainFragment extends Fragment {
     private StorageReference storageReference;
     private DatabaseReference databaseReference;
     private ImageView imageView;
+    private ImageView imageView2;
     private EditText editText;
     private Uri imguri;
+    private Uri imguri2;
     private Button btnBrowse_Click;
     private Button btnUpload;
     private Button btnShowListImage;
@@ -50,6 +54,9 @@ public class MainFragment extends Fragment {
     public static final String FB_Storage_Path = "image/";
     public static final String FB_Database_Path = "image";
     public static final int Request_Code = 1234;
+    public static final int Request_Code2 = 12345;
+
+    private List<Uri> listUri = new ArrayList<>();
 
 
     public MainFragment() {
@@ -74,6 +81,7 @@ public class MainFragment extends Fragment {
         databaseReference = FirebaseDatabase.getInstance().getReference(FB_Database_Path);
 
         imageView = (ImageView) rootView.findViewById(R.id.imageView);
+        imageView2 = (ImageView) rootView.findViewById(R.id.imageView2);
         editText = (EditText) rootView.findViewById(R.id.txtImageName);
         btnBrowse_Click = rootView.findViewById(R.id.btnBrowse_Click);
         btnBrowse_Click.setOnClickListener(new View.OnClickListener() {
@@ -83,6 +91,15 @@ public class MainFragment extends Fragment {
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(intent, "Select image"), Request_Code);
+            }
+        });
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select image"), Request_Code2);
             }
         });
 
@@ -98,9 +115,9 @@ public class MainFragment extends Fragment {
 
                     //Get the storage reference
                     StorageReference ref = storageReference.child(FB_Storage_Path + System.currentTimeMillis() + "." + getImageExt(imguri));
+                    final StorageReference ref2 = storageReference.child(FB_Storage_Path + System.currentTimeMillis() + "." + getImageExt(imguri2));
 
                     //Add file to reference
-
                     ref.putFile(imguri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
@@ -110,7 +127,7 @@ public class MainFragment extends Fragment {
                             dialog.dismiss();
                             //Display success toast msg
                             Toast.makeText(getContext(), "Image uploaded", Toast.LENGTH_SHORT).show();
-                            ImageUpload imageUpload = new ImageUpload(editText.getText().toString(), taskSnapshot.getDownloadUrl().toString());
+                            ImageUpload imageUpload = new ImageUpload(editText.getText().toString(), taskSnapshot.getDownloadUrl().toString(), ref2.getDownloadUrl().toString());
 
                             //Save image info in to firebase database
                             String uploadId = databaseReference.push().getKey();
@@ -165,10 +182,24 @@ public class MainFragment extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == Request_Code && resultCode == RESULT_OK && data != null && data.getData() != null) {
             imguri = data.getData();
+            listUri.add(imguri);
 
             try {
                 Bitmap bm = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imguri);
                 imageView.setImageBitmap(bm);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (requestCode == Request_Code2 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imguri2 = data.getData();
+            listUri.add(imguri);
+
+            try {
+                Bitmap bm = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imguri2);
+                imageView2.setImageBitmap(bm);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
